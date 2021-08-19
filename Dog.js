900ab662952985ebbe1108439a9ef236fcd2a4aa
tranthuong1999 <tranthuongpro99@gmail.com>
2021-08-19 16:33:44 +0530

    Sayhi method for gog

--- Dog.js
+++ Dog.js
@@ -1,6 +1,14 @@
-function Dog(){
+var chalk = require('chalk')
+
+
+function Dog(name){
 	this.stomatch = [] ;
+	this.name = name
 }
 Dog.prototype.eat = function(cat){
 	this.stomatch.push(cat);
 }
+Dog.prototype.sayHi = function(){
+	console.log(" Hi. I am a dog .My name is  " + chalk.red(this.name))
+}
+module.exports = Dog ;
--- Mouse.js
+++ Mouse.js
@@ -1,8 +1,4 @@
 function Mouse(name ){
 	this.name = name ;
-	this.dead = false 
 }
-Mouse.prototype.did = function(){
-	this.dead = true
-};
 module.exports = Mouse ;
